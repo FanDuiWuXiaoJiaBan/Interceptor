f08cf00e3e5f0abfa739ac575ca86e2753b40711
fuyong <1297570984@qq.com>
2020-03-18 12:26:30 +0800

    修改一些配置

--- requestinterceptor/src/main/java/com/buyaomiege/requestinterceptor/DefaultFormatPrinter.java
+++ requestinterceptor/src/main/java/com/buyaomiege/requestinterceptor/DefaultFormatPrinter.java
@@ -37,7 +37,7 @@ import okhttp3.Request;
  * ================================================
  */
 public class DefaultFormatPrinter implements FormatPrinter {
-    private static final String TAG = "lys";
+    private static final String TAG = "zfy";
     private static final String LINE_SEPARATOR = System.getProperty("line.separator");
     private static final String DOUBLE_SEPARATOR = LINE_SEPARATOR + LINE_SEPARATOR;
 
